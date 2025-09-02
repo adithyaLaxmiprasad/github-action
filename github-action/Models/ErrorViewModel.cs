f6be6208112ed5a5f9c30f910f2b6cb53aaaa9ea
WinLab11 <winlab@gmail.com>
2025-09-02 09:31:13 +0530

    Add project files.

---
+++ github-action/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace github_action.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
